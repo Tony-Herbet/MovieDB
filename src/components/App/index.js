23ed7f9d164530e6541e5945ccf670c4f47923bd
Tony-Herbet <herbet.le.faucheur.tony@gmail.com>
2021-07-23 17:25:56 +0200

    cra & libraries

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Basic install
+      </p>
+    </header>
+  </div>
+);
+
+export default App;
