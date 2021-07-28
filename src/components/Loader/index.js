b8da3f42c32408ae30a54fde3dd1f5c576eeee3d
Tony-Herbet <herbet.le.faucheur.tony@gmail.com>
2021-07-28 12:30:18 +0200

    loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import FilmReel from '../../assets/filmReel.svg';
+import './loader.scss';
+
 const Loader = () => (
-  <div>
-    Loading...
+  <div className="loader">
+    <div className="loader__text">Loading...</div>
+    <img className="loader__img" src={FilmReel} alt="loading animation" />
   </div>
 );
 
