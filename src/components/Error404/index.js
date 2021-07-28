11ae22733cbf52ffe90df8672a6840a00dcb7047
Tony-Herbet <herbet.le.faucheur.tony@gmail.com>
2021-07-28 13:37:02 +0200

    error 404

--- src/components/Error404/index.js
+++ src/components/Error404/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import ConfusedTravolta from '../../assets/confusedtravolta.jpg';
 import './error404.scss';
 
 const Error404 = () => (
   <div className="error404">
-    404
+    <h1 className="error404__title">
+      404 NOT FOUND
+    </h1>
+    <img
+      src={ConfusedTravolta}
+      alt="Confused Travolta as background"
+      className="error404__img"
+    />
   </div>
 );
 
